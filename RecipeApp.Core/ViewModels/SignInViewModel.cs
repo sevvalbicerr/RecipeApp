37bc285091813065b6b36dbdef00c706b570e7a2
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-04-18 13:45:44 +0300

    models added

---
+++ RecipeApp.Core/ViewModels/SignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApp.Core.ViewModel
+{
+    public class SignInViewModel
+    {
+        [Required(ErrorMessage ="Eposta giriniz!")]
+        [Display(Description ="Eposta")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
