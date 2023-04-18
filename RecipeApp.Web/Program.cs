7f86975b1b2f34b65a6fe914bdeb6494245f7be6
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-04-18 16:00:31 +0300

    Cookie Options

--- RecipeApp.Web/Program.cs
+++ RecipeApp.Web/Program.cs
@@ -19,6 +19,19 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     });
 });
 builder.Services.AddIdentityWithExt();
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    var cookieBuilder= new CookieBuilder();
+    cookieBuilder.Name= "AppCookie";
+
+    opt.LoginPath = new PathString("/Home/SignIn");
+    opt.Cookie=cookieBuilder;
+    opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+    opt.SlidingExpiration = true;
+    
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
