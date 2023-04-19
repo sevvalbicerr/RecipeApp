3c7bf898bf95c4c2cc3249d560e4b4f154da1293
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-04-19 10:58:09 +0300

    Profile page view added

---
+++ RecipeApp.Web/Controllers/MemberController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeApp.Core.Models;
+
+namespace RecipeApp.Web.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        private readonly SignInManager<AppUser> signInManager;
+
+        public MemberController(SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index() { 
+            return View();
+        }
+        public async Task<IActionResult> LogOut()
+        {
+           await signInManager.SignOutAsync();
+           return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            return View();
+        }
+
+
+
+    }
+}
