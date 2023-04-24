9c011d091742dcf7012c8d11c08ad52cfa6d0c71
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-04-24 11:03:24 +0300

    Generic Repo and Service added

---
+++ RecipeApp.Core/Repositories/Base/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace RecipeApp.Core.Repositories.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<IQueryable<T>> GetAllAsync();
+        //Buna belki gerek yoktur emin değilim.
+        Task<T> GetByIdAsync(int id);
+
+        Task<IQueryable<T>> WhereAsync(Expression<Func<T,bool>> expression);
+
+        void SaveChange();
+        Task SaveChangeAsync();
+
+
+    }
+}
---
+++ RecipeApp.Core/Services/Base/IService.cs
@@ -0,0 +1,23 @@
+﻿using RecipeApp.Core.DTOs;
+using RecipeApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp.Core.Services.Base
+{
+    public interface IService<InT,OutT> where InT : BaseEntity where OutT : class, new()
+    {
+        Task<OutT> AddAsync(OutT entity);
+        Task<NoContentDto> UpdateAsync(OutT entity);
+        Task<NoContentDto> DeleteAsync(int id);
+        Task<IQueryable<OutT>> GetAllAsync();
+        //Buna belki gerek yoktur emin değilim.
+        Task<OutT> GetByIdAsync(int id);
+
+        Task<IQueryable<OutT>> WhereAsync(Expression<Func<InT, bool>> expression);
+    }
+}
